fe507d73fcad3cc9b2f0465d514bba1f1ee8d915
Harrie de Groot <harrie@mannenvanwaarde.nl>
2018-08-17 23:22:28 +0200

    intouch driver with 4 button detection (default templates)

--- drivers/novy-intouch/device.js
+++ drivers/novy-intouch/device.js
@@ -19,47 +19,49 @@ module.exports = RFDevice => class NovyIntouchDevice extends RFDevice {
     // This function is "static" which means you cannot use 'this' inside this function!
      static payloadToData(payload) {
 
-        const data = {
-            address: this.bitArrayToString(payload),
-        };
-        data.id = data.address;
-        return data;
+         /** Signals
+         light: (18) [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1]
+         plus: (12) [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+         min: (12) [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0]
+         off: (18) [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1]
+         **/
 
-         //// Example implementation
-         //if (payload.length === 15) {
-         //    const data = {
-         //        address: util.bitArrayToString(payload.slice(0, 10)),
-         //        unit: util.bitArrayToString(payload.slice(10, 14)),
-         //        // A device capability id like onoff in the data object will automatically call this.setCapabilityValue.
-         //        onoff: payload[14] === 1,
-         //    };
-         //    // RFDriver requires an id property in the data object by default. This should be unique for every device instance
-         //    data.id = data.address + data.unit;
-         //    return data;
-         //}
-         //// If payload is not valid return null, payload will be discarded.
-         //return null;
+         if (payload.length === 12 || payload.length === 18) {
+             var address = util.bitArrayToString(payload.slice(0, 10));
+             if (address === '0101010101') {
+                 const data = {
+                     address: address,
+                     unit: util.bitArrayToString(payload.slice(10, payload.length)),
+
+                     // A device capability id like onoff in the data object will automatically call this.setCapabilityValue.
+                     light: payload.length === 18 && payload[16] === 0,
+                     plus: payload.length === 12 && payload[11] === 1,
+                     min: payload.length === 12 && payload[11] === 0,
+                     off: payload.length === 18 && payload[16] === 1,
+                 };
+                 // RFDriver requires an id property in the data object by default. This should be unique for every device instance
+                 data.id = data.address + data.unit;
+                 console.log(JSON.stringify(data));
+                 return data;
+             }
+         }
+         // If payload is not valid return null, payload will be discarded.
+         return null;
      }
 
      // If your driver handles raw payload requests instead of commands you need to implement the dataToPayload function below
      static dataToPayload(data) {
 
-        if (data && data.address && data.address.length === 18) {
-            return this.bitStringToBitArray(data.address);
-        }
-        return null;
-
-         //// Example implementation
-         //if (data.address && data.unit) {
-         //    return [].concat(
-         //        util.bitStringToBitArray(data.address),
-         //        util.bitStringToBitArray(data.unit),
-         //        // The default capabilityListener will send an object with the capability key set to its new value e.g. { onoff: true }
-         //        data.onoff ? 1 : 0
-         //    );
-         //}
-         //// If data is not valid return null, data object will be discarded.
-         //return null;
+         if (data.address && data.unit) {
+             return [].concat(
+                 util.bitStringToBitArray(data.address),
+                 util.bitStringToBitArray(data.unit),
+                 // The default capabilityListener will send an object with the capability key set to its new value e.g. { onoff: true }
+                 data.onoff ? 1 : 0
+             );
+         }
+         // If data is not valid return null, data object will be discarded.
+         return null;
      }
 
     // If your driver uses the rf.program, rf.codewheel or rf.dipswitch pair template you need to implement the generateData function below
