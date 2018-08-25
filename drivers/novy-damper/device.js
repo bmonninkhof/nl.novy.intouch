3e8d091bdf57ab26e598847e79117484a6b0826b
Harrie de Groot <>
2018-08-25 21:40:49 +0200

    damper signals

--- drivers/novy-damper/device.js
+++ drivers/novy-damper/device.js
@@ -6,7 +6,6 @@ const Signal = require('../../lib/NovyIntouchSignal.js').Signal;
 
 module.exports = RFDevice => class NovyIntouchLDamperDevice extends NovyIntouchDevice(RFDevice) {
 
-
     //onRFInit() {
     //    super.onRFInit();
 
@@ -14,132 +13,143 @@ module.exports = RFDevice => class NovyIntouchLDamperDevice extends NovyIntouchD
     //    //this.toggleCapabilityValue = {};
     //}
 
-    //static payloadToData(payload) {
-    //    var data = super.payloadToData(payload);
-    //    debugger;
-    //    if (data) {
-            
-          
-
-    //        //data.damper = unit == Signal.increase ? 'increase' : unit == Signal.decrease ? 'decrease' : unit == Signal.off ? 'off' : undefined;
-    //        //data.light = unit == Signal.light ? 'light' : unit == Signal.off ? 'off' : undefined;
-
-    //        //data.channel_up = unit == Signal.increase;
-    //        //data.channel_down = unit == Signal.decrease;
-    //        //data.onoff = unit == Signal.light ? true : unit == Signal.off ? false : undefined;
-    //    }
-    //    return data;
-    //}
-
-    static dataToPayload(data) {
-        if (data) {
-
-            //var action = data.damper || data.light;
-            //if (action && Signal[action]) {
-            //    return util.bitStringToBitArray(Signal.address + Signal[action]);
-            //}
-            //if (action.channel_up) {
-            //    return util.bitStringToBitArray(Signal.address + Signal.increase);
-            //}
-            //if (action.channel_down) {
-            //    return util.bitStringToBitArray(Signal.address + Signal.decrease);
-            //}
-            //if (action.onoff === false) {
-            //    return util.bitStringToBitArray(Signal.address + Signal.off);
-            //}
-            //if (action.onoff === true) {
-            //    // TODO: conditional?
-            //    return util.bitStringToBitArray(Signal.address + Signal.light);
-            //}
-        }
-        return super.dataToPayload(data);
-    }
-
     static generateData() {
         debugger;
         const data = super.generateData();
         data.onoff = false;
-        data.light = 'off';
-        data.damper = 'speed_0';
+        data.light = false;
+        data.damper = 'off';
         return data;
     }
 
-    //assembleDeviceObject() {
-    //    // Ignore check for group button
-    //    return super.assembleDeviceObject(true);
-    //}
-
     parseIncomingData(data) {
-        data = super.parseIncomingData(data);
         //debugger;
+        console.info("parseIncomingData", data);
+        if (data) {
+            delete data._sendOptions;
 
-        const state = this.deviceState.lastFrame;
-        data.onoff = state.onoff === true ? true : false;
-        data.light = state.light === "on" ? "on" : "off";
-        data.damper = state.damper || 'speed_0';
+            const state = this.deviceState.lastFrame;
+            data.onoff = Boolean(state.onoff);
+            data.light = Boolean(state.light);
 
-        switch (data.unit) {
-            case Signal.light:
-                data.light = state.light === "on" ? "off" : "on";
-                break;
-            case Signal.increase:
-                data.damper = 'increase';
-                break;
-            case Signal.decrease:
-                data.damper = 'decrease';
-                break;
-            case Signal.onoff:
-                //data.onoff = state.onoff == "1" ? "0" : "1";
-                data.onoff = !state.onoff;
-                break;
+            if (!data.damper) {
+                switch (data.unit) {
+                    case Signal.light:
+                        data.light = !data.light;
+                        data.damper = data.light ? 'light_on' : 'light_off';
+                        break;
+                    case Signal.increase:
+                        data.speed = Math.min(4, Math.max(0, Number(data.speed || 0) + 1));
+                        data.damper = 'increase';
+                        break;
+                    case Signal.decrease:
+                        data.speed = Math.min(4, Math.max(0, Number(data.speed || 0) - 1));
+                        data.damper = 'decrease';
+                        break;
+                    case Signal.onoff:
+                        data.onoff = !state.onoff;
+                        data.damper = data.onoff ? 'on' : 'off';
+                        break;
+                }
+            }
         }
 
-        return data;
+        return super.parseIncomingData(data);
     }
 
     getData() {
-        // HACK: Fix for unmatching ids @ send dataCheck due unit change
-        return this._data ? JSON.parse(JSON.stringify(this._data)) : super.getData();
+        // HACK: Fix for unmatching ids @ send dataCheck due unit change => should be handled by matchesData call
+        return this._data ? Object.assign({}, this._data) : super.getData();
     }
 
     getSendOptionsForData(data) {
-        if (data.unit == Signal.onoff && !data.onoff) {
-            return { repetitions: 2 }; // Not working...needs a delay
-        }
+        console.info("getSendOptionsForData", data);
+        return data ? data._sendOptions || null : null;
     }
 
-    parseOutgoingData(data) {
-        
+    assembleSendData(data) {
+        console.info("assembleSendData", data);
         //debugger;
 
-        const state = this.deviceState.lastFrame;
+        if (data) {
 
-        if (state.onoff != data.onoff) {
-            data.unit = Signal.onoff;
-        } else if (data.light != state.light) {
-            data.unit = Signal.light;
-        } else {
+            delete data._sendOptions;
+
+            // map onoff to damper action
+            if (data.onoff !== undefined) {
+                data.damper = data.onoff ? 'on' : 'off';
+                delete data.onoff; // delete to fetch from current state (lastFrame)
+            }
+
+            // assemble
+            data = super.assembleSendData(data);
+
+            let state = this.deviceState.lastFrame;
+            data.onoff = Boolean(state.onoff); // TODO: Create reset override/read from settings?
+            data.light = Boolean(state.light);
+
+            // set action
             switch (data.damper) {
+                case 'on':
+                    if (data.onoff) {
+                        return null;
+                    }
+                    data.onoff = true;
+                    data.unit = Signal.onoff;
+                    break;
+                case 'off':
+                    if (!data.onoff) {
+                        return null;
+                    }
+                    data.onoff = false;
+                    data.unit = Signal.onoff;
+                    data._sendOptions = { repetitions: 1 }; // off = dubble press (skip timeout)
+                    break;
+                case 'light_on':
+                    if (data.light) {
+                        return null;
+                    }
+                    data.light = true;
+                    data.unit = Signal.light;
+                    break;
+                case 'light_off':
+                    if (!data.light) {
+                        return null;
+                    }
+                    data.light = false;
+                    data.unit = Signal.light;
+                    break;
                 case 'increase':
-                    state.unit = Signal.increase;
+                    data.speed = Math.min(4, Math.max(0, (data.speed || 0) + 1));
+                    data.unit = Signal.increase;
                     break;
                 case 'decrease':
-                    state.unit = Signal.decrease;
+                    data.speed = Math.min(4, Math.max(0, (data.speed || 0) - 1));
+                    data.unit = Signal.decrease;
                     break;
                 case 'speed_0':
                 case 'speed_1':
                 case 'speed_2':
                 case 'speed_3':
                 case 'speed_4':
-                    let speed = Number(state.damper.substr(6));
-                    // handle speed
+                    let speed = Number(data.damper.substr(6));
+                    if (data.speed === speed) {
+                        return null;
+                    }
+                    if (speed > data.speed) {
+                        data.unit = Signal.increase;
+                        data._sendOptions = { repetitions: Math.min(3, Math.max(0, speed - data.speed - 1)) }
+                    } else if (speed < data.speed) {
+                        data.unit = Signal.decrease;
+                        data._sendOptions = { repetitions: Math.min(3, Math.max(0, data.speed - speed - 1)) }
+                    }
                     break;
             }
         }
-        
+
         // FIX: register data to fix id mismatches due unit change
         this._data = data;
 
-        return super.parseOutgoingData(data);
+        return data;
     }
 };
